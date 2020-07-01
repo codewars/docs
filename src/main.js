14b943d34e2904657ceefb9810b40d8cfb97675c
kazk <kazk.dev@gmail.com>
2020-06-30 20:38:48 -0700

    Move global styles out of the layout

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
+import "@/assets/styles.scss";
 import DefaultLayout from "@/layouts/Default.vue";
 
 export default function (Vue, { router, head, isClient }) {
