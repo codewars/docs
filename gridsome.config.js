4fca5fbf775f0a7e453cfdc4df5efe4890e27551
kazk <kazk.dev@gmail.com>
2020-07-01 19:56:08 -0700

    Remove Google Analytics plugin

--- gridsome.config.js
+++ gridsome.config.js
@@ -55,13 +55,6 @@ module.exports = {
     },
 
     {
-      use: "@gridsome/plugin-google-analytics",
-      options: {
-        id: process.env.GA_ID ? process.env.GA_ID : "XX-999999999-9",
-      },
-    },
-
-    {
       use: "@gridsome/plugin-sitemap",
       options: {},
     },
