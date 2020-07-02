b2e28c2a55279bcbe4badce4cecd9ccfa2f19b05
kazk <kazk.dev@gmail.com>
2020-07-02 15:44:01 -0700

    Clean up Sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,11 +1,10 @@
 <template>
-  <div ref="sidebar" v-if="showSidebar" class="px-4 pt-8 lg:pt-12">
-    <div
-      v-for="(section, index) in sidebar.sections"
-      :key="section.title"
-      class="pb-4 mb-4 border-ui-border"
-      :class="{ 'border-b': index < sidebar.sections.length - 1 }"
-    >
+  <div
+    ref="sidebar"
+    v-if="showSidebar"
+    class="px-4 pt-8 lg:pt-12 divide-y border-ui-border"
+  >
+    <div v-for="section of sidebar.sections" :key="section.title" class="py-4">
       <h3
         class="pt-0 mt-0 mb-1 font-black text-sm leading-snug tracking-tight uppercase border-none text-ui-typo"
       >
@@ -17,13 +16,10 @@
           v-for="page in findPages(section.items)"
           :id="page.path"
           :key="page.path"
-          :class="getClassesForAnchor(page)"
+          :class="getClassesForAnchor(page.path)"
           @mousedown="$emit('navigate')"
         >
-          <g-link
-            :to="`${page.path}`"
-            class="flex items-center py-1 font-semibold"
-          >
+          <g-link :to="page.path" class="flex items-center py-1 font-semibold">
             <span
               class="absolute w-2 h-2 -ml-3 rounded-full opacity-0 bg-ui-primary transition transform scale-0 origin-center"
               :class="{
@@ -78,11 +74,11 @@ export default {
     },
   },
   methods: {
-    getClassesForAnchor({ path }) {
+    getClassesForAnchor(path) {
+      const current = this.currentPage.path === path;
       return {
-        "text-ui-primary": this.currentPage.path === path,
-        "transition transform hover:translate-x-1 hover:text-ui-primary":
-          !this.currentPage.path === path,
+        "text-ui-primary": current,
+        "transition transform hover:translate-x-1 hover:text-ui-primary": !current,
       };
     },
     findPages(links) {
