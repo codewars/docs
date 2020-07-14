822ed0e0583eba3e4174eb36e832ec59880f5b86
kazk <kazk.dev@gmail.com>
2020-07-14 13:51:32 -0700

    Test GitHub Actions cache

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: "Codewars Docs",
+  siteName: 'Codewars Docs',
   icon: {
     favicon: "./src/assets/favicon.png",
     touchicon: "./src/assets/favicon.png",
