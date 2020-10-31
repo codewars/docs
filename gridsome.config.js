d8f03a1ac9899cac6a134f121e628d57ab0494ac
kazk <kazk.dev@gmail.com>
2020-10-30 22:54:10 -0700

    Remove files from docc starter

--- gridsome.config.js
+++ gridsome.config.js
@@ -22,24 +22,6 @@ module.exports = {
     },
     sidebar: [
       {
-        name: "docs",
-        sections: [
-          {
-            title: "Getting Started",
-            items: [
-              "/docs/",
-              "/docs/installation/",
-              "/docs/writing-content/",
-              "/docs/deploying/",
-            ],
-          },
-          {
-            title: "Configuration",
-            items: ["/docs/settings/", "/docs/sidebar/"],
-          },
-        ],
-      },
-      {
         name: "getting-started",
         sections: [
           {
