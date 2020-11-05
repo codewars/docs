e71b35310b51d08b57ebae917308bc2aba8d6772
kazk <kazk.dev@gmail.com>
2020-11-04 20:12:55 -0800

    Use scroll-margin-top (#160)

--- src/main.js
+++ src/main.js
@@ -7,6 +7,25 @@ export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component("Layout", DefaultLayout);
 
+  // The default Gridsome behaviour with a fix to make `scroll-margin-top` work.
+  // https://github.com/gridsome/gridsome/issues/1361#issuecomment-709534494
+  router.options.scrollBehavior = async (to, from, saved) => {
+    if (saved) return saved;
+
+    if (to.hash) {
+      const elem = document.querySelector(to.hash);
+      if (elem) {
+        return {
+          selector: to.hash,
+          offset: { y: parseFloat(getComputedStyle(elem).scrollMarginTop) },
+        };
+      }
+      return { selector: to.hash };
+    }
+
+    return { x: 0, y: 0 };
+  };
+
   router.beforeEach((to, _from, next) => {
     head.meta.push({
       key: "og:url",
