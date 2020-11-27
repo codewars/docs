4612fd189ed4cb1932c9efd226fd4e7aa67b33e1
kazk <kazk.dev@gmail.com>
2020-11-26 20:50:19 -0800

    Update default values of typography plugin
    Also, reorder to match the plugin to make it easier to follow.

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,14 +42,10 @@ module.exports = {
             // color: theme("colors.gray.700"),
             color: theme("colors.typo.base"),
             '[class~="lead"]': {
-              // color: theme("colors.gray.700"),
+              // color: theme("colors.gray.600"),
               color: theme("colors.typo.base"),
             },
             a: {
-              // color: theme("colors.gray.900"),
-              color: theme("colors.typo.high"),
-            },
-            "a code": {
               // color: theme("colors.gray.900"),
               color: theme("colors.typo.high"),
             },
@@ -58,21 +54,21 @@ module.exports = {
               color: theme("colors.typo.high"),
             },
             "ol > li::before": {
-              // color: theme("colors.gray.600"),
+              // color: theme("colors.gray.500"),
               color: theme("colors.typo.low"),
             },
             "ul > li::before": {
-              // backgroundColor: theme("colors.gray.400"),
+              // backgroundColor: theme("colors.gray.300"),
               backgroundColor: theme("colors.ui.border"),
             },
             hr: {
-              // borderColor: theme("colors.gray.300"),
+              // borderColor: theme("colors.gray.200"),
               borderColor: theme("colors.ui.border"),
             },
             blockquote: {
               // color: theme("colors.gray.900"),
               color: theme("colors.typo.base"),
-              // borderLeftColor: theme("colors.gray.300"),
+              // borderLeftColor: theme("colors.gray.200"),
               borderLeftColor: theme("colors.ui.border"),
             },
             h1: {
@@ -92,14 +88,8 @@ module.exports = {
               color: theme("colors.typo.high"),
             },
             "figure figcaption": {
-              // color: theme("colors.gray.600"),
+              // color: theme("colors.gray.500"),
               color: theme("colors.typo.low"),
-            },
-            "code::before": {
-              content: "none",
-            },
-            "code::after": {
-              content: "none",
             },
             code: {
               // color: theme("colors.gray.900"),
@@ -110,20 +100,36 @@ module.exports = {
               borderRadius: theme("borderRadius.DEFAULT"),
               fontWeight: null,
             },
+            "code::before": {
+              content: "none",
+            },
+            "code::after": {
+              content: "none",
+            },
+            "a code": {
+              // color: theme("colors.gray.900"),
+              color: theme("colors.typo.high"),
+            },
             pre: {
-              // color: theme("colors.gray.300"),
+              // color: theme("colors.gray.200"),
               color: theme("colors.typo.high"),
               // backgroundColor: theme("colors.gray.800"),
               backgroundColor: "inherit",
             },
+            "pre code::before": {
+              content: "none",
+            },
+            "pre code::after": {
+              content: "none",
+            },
             thead: {
               // color: theme("colors.gray.900"),
               color: theme("colors.typo.high"),
-              // borderBottomColor: theme("colors.gray.400"),
+              // borderBottomColor: theme("colors.gray.300"),
               borderBottomColor: theme("colors.ui.border"),
             },
             "tbody tr": {
-              // borderBottomColor: theme("colors.gray.300"),
+              // borderBottomColor: theme("colors.gray.200"),
               borderBottomColor: theme("colors.ui.border"),
             },
           },
