1f93e797112d10290aa6cde67fd3116c449eda58
hobovsky <hobson@wp.pl>
2021-01-02 15:14:23 +0100

    navigation

--- gridsome.config.js
+++ gridsome.config.js
@@ -170,7 +170,7 @@ module.exports = {
             title: "User Interface",
             items: [
               "/references/gui/",
-              "/references/gui/kata-creator/",
+              "/references/gui/kata-editor/",
               "/references/gui/kata-trainer/",
             ],
           },
