bdb5b59da13c3c75117a8b80145b32066b29f90d
kazk <kazk.dev@gmail.com>
2021-04-15 02:18:35 -0700

    Remove `bold` prop and style from outside

--- src/components/Anchored.jsx
+++ src/components/Anchored.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Anchored = ({ id, bold = true, children }) => {
+const Anchored = ({ id, children }) => {
   return (
     <div id={`.${id}`} class="anchored">
       <a class="anchor-link" href={`#.${id}`} aria-hidden="true"></a>
-      {bold ? <b>{children}</b> : children}
+      {children}
     </div>
   );
 };
