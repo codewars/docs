6f839572ef51678a285ca6dc136651188b89533e
kazk <kazk.dev@gmail.com>
2022-06-23 16:10:26 -0700

    Change code block theme and support light mode

--- src/remark/shiki/index.js
+++ src/remark/shiki/index.js
@@ -26,9 +26,9 @@ const remapLanguageId = (id) => {
 // Use shiki for syntax highlighting.
 // TODO Decoration. Like Docusaurus builtin, or https://github.com/shikijs/shiki/issues/5
 //      or https://github.com/andrewbranch/gatsby-remark-vscode#line-highlighting
-const createHighlighter = ({ theme = "nord" } = {}) => {
+const createHighlighter = ({ themes = ["nord"] } = {}) => {
   // Reuse the same instance
-  const highlighterPromise = getHighlighter({ theme }).then(
+  const highlighterPromise = getHighlighter({ themes }).then(
     async (highlighter) => {
       const langs = [
         // See languages/README.md for sources.
@@ -65,62 +65,68 @@ const createHighlighter = ({ theme = "nord" } = {}) => {
 
   return () => async (tree) => {
     const highlighter = await highlighterPromise;
-    const fg = highlighter.getForegroundColor();
-    const bg = highlighter.getBackgroundColor();
 
     visit(tree, "code", (node) => {
-      // const meta = node.meta;
-      const lang = node.lang || "text";
-      const code = node.value;
-      let tokenLines = null;
-      try {
-        tokenLines = highlighter.codeToThemedTokens(
-          code,
-          remapLanguageId(lang)
-        );
-      } catch (e) {
-        if (/^No language registration for/.test(e.message)) {
-          console.warn(`shiki: ${e.message}`);
-          console.warn(`shiki: Falling back to "text"`);
-          tokenLines = highlighter.codeToThemedTokens(code, "text");
-        } else {
-          throw e;
-        }
-      }
-
-      let html = `<pre class="${theme}" style="background-color: ${bg}">`;
-      html += `<code class="language-${lang}">`;
-      for (const line of tokenLines) {
-        html += `<span>`;
-        for (const token of line) {
-          const css = [`color: ${token.color || fg}`];
-          switch (token.fontStyle) {
-            case -1: // NotSet
-            case 0: // None
-              break;
-            case 1: // Italic
-              css.push(`font-style: italic`);
-              break;
-            case 2: // Bold
-              css.push(`font-weight: bold`);
-              break;
-            case 4: // Underline
-              css.push(`text-decoration: underline`);
-              break;
-          }
-          const content = escapeHtml(token.content);
-          html += `<span style="${css.join("; ")}">${content}</span>`;
-        }
-        html += `</span>\n`;
-      }
-
-      html = html.replace(/\n*$/, ""); // get rid of trailing new lines
-      html += `</code></pre>`;
-
-      node.value = html;
+      node.value = themes
+        .map((theme) =>
+          codeToHtml(highlighter, node.value, node.lang || "text", theme)
+        )
+        .join("");
       node.type = "html";
     });
   };
+};
+
+const codeToHtml = (highlighter, code, lang, theme) => {
+  let tokenLines = null;
+  try {
+    tokenLines = highlighter.codeToThemedTokens(
+      code,
+      remapLanguageId(lang),
+      theme
+    );
+  } catch (e) {
+    if (/^No language registration for/.test(e.message)) {
+      console.warn(`shiki: ${e.message}`);
+      console.warn(`shiki: Falling back to "text"`);
+      tokenLines = highlighter.codeToThemedTokens(code, "text", theme);
+    } else {
+      throw e;
+    }
+  }
+
+  const fg = highlighter.getForegroundColor(theme);
+  const bg = highlighter.getBackgroundColor(theme);
+
+  let html = `<pre class="${theme}" style="background-color: ${bg}">`;
+  html += `<code class="language-${lang}">`;
+  for (const line of tokenLines) {
+    html += `<span>`;
+    for (const token of line) {
+      const css = [`color: ${token.color || fg}`];
+      switch (token.fontStyle) {
+        case -1: // NotSet
+        case 0: // None
+          break;
+        case 1: // Italic
+          css.push(`font-style: italic`);
+          break;
+        case 2: // Bold
+          css.push(`font-weight: bold`);
+          break;
+        case 4: // Underline
+          css.push(`text-decoration: underline`);
+          break;
+      }
+      const content = escapeHtml(token.content);
+      html += `<span style="${css.join("; ")}">${content}</span>`;
+    }
+    html += `</span>\n`;
+  }
+
+  html = html.replace(/\n*$/, ""); // get rid of trailing new lines
+  html += `</code></pre>`;
+  return html;
 };
 
 module.exports = { createHighlighter };
